706c0f93911e17dce1c4ef8e98a0180c236bd318
Eric Fox <ericliamhjx@gmail.com>
2019-08-20 13:20:01 +1000

    Initial commit

---
+++ app/src/main/java/com/example/expireddatetracker/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.expireddatetracker;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.example.expireddatetracker.Fragments.HomeFragment;
+import com.example.expireddatetracker.Fragments.NotificationFragment;
+import com.example.expireddatetracker.Fragments.TrackFragment;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.navigation_dashboard:
+                    fragment = new TrackFragment();
+                    break;
+                case R.id.navigation_notifications:
+                    fragment = new NotificationFragment();
+                    break;
+            }
+            return loadFragment(fragment);
+        }
+    };
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadFragment(new HomeFragment());
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
